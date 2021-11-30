c16e7a3ae6175f6dd04f3374bdcda2c7ef08d31e
ferdi <ferdi.maillist@gmail.com>
2021-11-30 23:04:33 +0100

    add client and server downloaded from spring-initializr

---
+++ client/src/test/java/com/ferdisonmezay/websocket/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferdisonmezay.websocket.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
